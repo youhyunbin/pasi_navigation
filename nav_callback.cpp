c17b5b339e12e7270ae13edd09f8300f5b46e740
Elena <102175112+rubynote@users.noreply.github.com>
2022-05-25 00:18:22 +0900

    Update nav_callback.cpp

--- nav_callback.cpp
+++ nav_callback.cpp
@@ -1,10 +1,8 @@
-//Navigation
+#include <fstream>
+#include <iostream>
+
 #include "moveToGoal.h"
 #include <std_msgs/Int16.h>
-
-//File read & write
-#include <iostream>
-#include <fstream>
 
 using namespace std;
 
@@ -28,8 +26,50 @@ public:
     ROS_INFO("Received : %d", rcvForwardResult.data);
     if(rcvForwardResult.data == 2)
     {
-      Second.data = 102;
-      pubMotor.publish(Second);
+      ifstream order("/home/hyun/navigation/src/navigation/src/order.txt");
+
+      int j;
+      int list[16];
+      int count = 0; int count1 = 0; int count2 = 0;
+      int k=0; int n=0; int t=0;
+
+      fill_n(list,16,0); // Initiate list to "0"
+
+      for (j=0;j<16;j++)
+      {
+        order >> list[j];
+        if(list[j] == 2) count++;
+        if(list[j] == 3) count1++;
+        if(list[j] == 4) count2++;
+      }
+
+      if(count != 0){
+        while(k < count){
+          First.data = 101;
+          pubMotor.publish(First);
+          sleep(5);
+          k++;
+        }
+      }
+
+      if(count1 != 0){
+        while(n < count1){
+          Second.data = 102;
+          pubMotor.publish(Second);
+          sleep(5);
+          n++;
+        }
+      }
+
+      if(count2 != 0){
+        while(t < count2){
+          Third.data = 103;
+          pubMotor.publish(Third);
+          sleep(5);
+          t++;
+        }
+      }
+      order.close();
     }
   }
 
@@ -39,10 +79,7 @@ public:
     if(rcvMotorResult.data == 3)
     {
       Backward.data = 2;
-      pubUltraSonic2.publish(Backward);
-    }
-    else {
-      ROS_INFO("Failed");
+      pubUltraSonic.publish(Backward);
     }
   }
 
@@ -51,11 +88,21 @@ public:
     ROS_INFO("Received : %d", rcvBackwardResult.data);
     if(rcvBackwardResult.data == 4)
     {
+      ifstream floors("/home/hyun/navigation/src/navigation/src/floor.txt");
+
+      int i;
+      double number[16];
+
+      for(i=0;i<16;i++)
+      {
+        floors >> number[i];
+      }
+
       ROS_INFO("Moving to Packing Place");
-      ROS_INFO("Packing Place Reached");
-    }
-    else {
-      ROS_INFO("Failed");
+      goalReached = moveToGoal(number[4],number[5],number[6],number[7]);
+      if(goalReached){
+        ROS_INFO("Packing Place Reached");
+      }
     }
   }
 
@@ -77,19 +124,19 @@ private:
   std_msgs::Int16 First;
   std_msgs::Int16 Second;
   std_msgs::Int16 Third;
-
-  int list[100];
-  double number[16];
 };
+
 
 int main(int argc, char** argv){
   //Initiate ROS
   ros::init(argc, argv, "nav_callback");
+
   ROS_INFO("Navigation_Callback connected");
 
-  //Create an object of class NavCore
+  //Create an object of class Navcall
   NavCall navCall;
 
   ros::spin();
+
   return 0;
 }
